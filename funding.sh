fcf6875d40f24160b644895c9be9c124dcb911dc
Standinwol <toantrum9x@gmail.com>
2024-09-27 17:16:24 +0700

    Update funding.sh

--- funding.sh
+++ funding.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-urls=("https://allora-rpc.testnet.allora.network/"
-    "https://allora-testnet-rpc.itrocket.net/"
-    "https://rpc.ankr.com/allora_testnet"
-    "http://66.70.177.125:27657/")
-
-num_urls=${#urls[@]}
-
 echo "Kiem tra vi funding"
 funding=$(allorad keys show fundingWl -a --keyring-backend test 2>/dev/null)
 if [ -z "$funding" ]; then
@@ -20,20 +13,19 @@ fi
 
 while IFS="|" read -r wallet mnemonic
 do
-    echo "================="
-    echo "Checking wallet: $wallet"
-    random_index=$(( RANDOM % num_urls ))
-    NODE_URL=${urls[$random_index]}
-    
-    BAL=$(allorad q bank balances "$wallet" --node $NODE_URL -o json | jq -r '.balances[] | select(.denom=="uallo") | .amount')
+    echo "========Checking $wallet========="
+    NODE_URL="https://rpc.ankr.com/allora_testnet"
+    echo "Using $NODE_URL"
+
+    BAL=$(allorad q bank balances $wallet --node $NODE_URL -o json | jq -r '.balances[] | select(.denom=="uallo") | .amount')
+    sleep 1
     echo "Balance: $BAL"
-    
     if [ -z "$BAL" ]; then
-        echo "Balance is empty. Sending 1000000000000000uallo to $wallet..."
+        echo "Sending..."
         allorad tx bank send fundingWl $wallet 1000000000000000uallo --chain-id allora-testnet-1 --keyring-backend test --node $NODE_URL --gas-prices 1000000uallo --gas 100000 -y
         sleep 5
     else
-        echo "Wallet $wallet already has balance. No need to send."
+        echo "No need to send"
     fi
 
 done < "$HOME/wl_formated.txt"
